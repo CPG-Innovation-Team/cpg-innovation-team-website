a64babd375b029950a3b4a7ee78f13e036f89656
fork1028 <xinyue_chen@icloud.com>
2021-06-24 13:35:36 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/aboutUs">联系我们</router-link>
+      <router-link to="/">Home</router-link> | <router-link to="/aboutUs">联系我们</router-link> |
+      <router-link to="/teamInfo">团队介绍</router-link>
     </div>
     <router-view />
   </v-app>
