01953ffc2d24f8a64ae9d7071ea05e5efec02da6
fork1028 <xinyue_chen@icloud.com>
2021-06-25 17:28:11 +0800

    add scroll behaviour in router.js

--- src/router.js
+++ src/router.js
@@ -40,6 +40,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior() {
+    return {
+      x: 0,
+      y: 0,
+      behavior: 'smooth',
+    };
+  },
 });
 
 export default router;
