530391a6f8c28e9aaa2276e1d6639a28868a31f1
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-07-05 21:28:11 -0700

    changed style on nav search

--- tests/unit/HeaderNav.spec.js
+++ tests/unit/HeaderNav.spec.js
@@ -1,9 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
 import HeaderNav from '@/components/HeaderNav.vue';
 
-describe('HelloWorld.vue', () => {
+describe('test header', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
+    const msg = 'home';
     const wrapper = shallowMount(HeaderNav, {
       propsData: { msg },
     });
