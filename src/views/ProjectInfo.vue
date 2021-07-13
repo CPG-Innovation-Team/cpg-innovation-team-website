e5eaf7e7ffd2792c409519f11accc8f562f68fb5
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-07-13 15:02:51 -0700

    finished mobile version for project page

--- src/views/ProjectInfo.vue
+++ src/views/ProjectInfo.vue
@@ -4,7 +4,7 @@
       <v-img src="https://picsum.photos/500/300?image=91" id="project-head-img" max-height="400" />
 
       <v-container>
-        <v-row>
+        <v-row class="hidden-sm-and-down">
           <v-col class="project-col" v-for="(project, i) in projects" :key="i">
             <v-hover v-slot="{ hover }">
               <v-card class="mx-auto" color="grey lighten-4" max-width="600">
@@ -24,6 +24,30 @@
             </v-hover>
           </v-col>
         </v-row>
+
+        <row v-for="(project, i) in projects" :key="i" class="mobile hidden-md-and-up">
+          <v-img :aspect-ratio="4 / 1" :src="project.img">
+            <v-row>
+              <v-col cols="8">
+                <v-card-title class="header">{{ project.title }}</v-card-title>
+              </v-col>
+              <v-col cols="4">
+                <v-chip icon @click="project.show = !project.show" class="chip-show">
+                  更多<v-icon>{{ project.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-chip>
+              </v-col>
+            </v-row>
+          </v-img>
+
+          <v-expand-transition>
+            <v-card v-show="project.show">
+              <v-card-subtitle>介绍</v-card-subtitle>
+              <v-card-text>{{ project.intro }}</v-card-text>
+              <v-card-subtitle>项目</v-card-subtitle>
+              <v-chip v-for="(link, j) in project.list" :key="j" color="blue" class="chip-link"> {{ link }} </v-chip>
+            </v-card>
+          </v-expand-transition>
+        </row>
       </v-container>
     </v-container>
   </div>
@@ -38,16 +62,25 @@ export default {
         title: 'Lorem Ipsum',
         img: 'https://picsum.photos/500/300?image=60',
         list: ['item1', 'item2', 'item3'],
+        intro:
+          "I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.",
+        show: false,
       },
       {
         title: 'Lorem Ipsum',
         img: 'https://picsum.photos/500/300?image=62',
         list: ['item4', 'item5', 'item6'],
+        intro:
+          "I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.",
+        show: false,
       },
       {
         title: 'Lorem Ipsum',
         img: 'https://picsum.photos/500/300?image=65',
         list: ['item7', 'item8', 'item9'],
+        intro:
+          "I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.",
+        show: false,
       },
     ],
   }),
@@ -71,6 +104,23 @@ h1 {
       margin: 20px;
       font-size: 1.2em;
     }
+  }
+}
+
+.mobile {
+  .header {
+    color: white;
+    margin-top: 5%;
+  }
+  .chip-show {
+    float: right;
+    margin: 25% 10%;
+  }
+
+  .chip-link {
+    color: white;
+    margin: 10px;
+    padding: 10px 20px;
   }
 }
 </style>
