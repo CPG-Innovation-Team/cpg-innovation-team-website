61e67170e13ba4ac04a89c6a001ab3221ba10c13
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-07-13 15:17:54 -0700

    delete comments in unit test

--- tests/unit/components/HeaderNav.spec.js
+++ tests/unit/components/HeaderNav.spec.js
@@ -13,24 +13,13 @@ Vue.use(Vuetify);
 Vue.use(VueI18n);
 Vue.use(VueRouter);
 
-// console.log(i18n.messages);
-
 describe('Navigation bar', () => {
   let wrapper;
-  // const mockRoute = {
-  //   params: {
-  //     id: 1,
-  //   },
-  // };
-  // const mockRouter = {
-  //   push: jest.fn(),
-  // };
+
   beforeEach(() => {
     wrapper = mount(HeaderNav, {
       mocks: {
         $t: (msg) => i18n.messages['zh-CN'][msg],
-        // $router: mockRouter,
-        // $route: mockRoute,
       },
       stubs: ['router-link', 'router-view'],
       vuetify,
@@ -58,21 +47,5 @@ describe('Navigation bar', () => {
     const drawer = wrapper.find('.v-btn__content .v-icon');
     drawer.trigger('click');
     expect(wrapper.vm.drawer).toBe(true);
-
-    // console.log(wrapper.html());
-    // const button = wrapper.findAll('.nav-link .v-btn__content').at(0);
-    // console.log('text: ', button.html());
-    // button.trigger('click');
-    // expect(wrapper.vm.drawer).toBe(false);
-  });
-
-  it('change language', () => {
-    // console.log(wrapper.vm.lang);
-    // const button = wrapper.findAll('.language-setting').at(0);
-    // button.trigger('click');
-    // console.log(wrapper.html());
-    // console.log(wrapper.classes('.v-list-item__content'));
-    // expect(wrapper.classes()).toContain('language-setting');
-    // expect(wrapper.vm.lang).toBe('Eng');
   });
 });
