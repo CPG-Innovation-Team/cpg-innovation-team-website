d6fbcd58c6c583ece392bd25378a4748f15e3578
fork1028 <xinyue_chen@icloud.com>
2021-07-14 11:33:16 +0800

    add util methods in recruitment info page

--- src/util.js
+++ src/util.js
@@ -2,4 +2,13 @@ export default {
   getPageLength(arr) {
     return Math.ceil(arr.length / 10);
   },
+  sort(arr) {
+    const temp = arr.concat([]);
+    temp.sort((a, b) => {
+      const t1 = new Date(Date.parse(a.time.replace(/-/g, '/')));
+      const t2 = new Date(Date.parse(b.time.replace(/-/g, '/')));
+      return t2.getTime() - t1.getTime();
+    });
+    return temp;
+  },
 };
--- tests/unit/views/RecruitmentInfo.spec.js
+++ tests/unit/views/RecruitmentInfo.spec.js
@@ -9,29 +9,6 @@ const router = new VueRouter();
 describe('Recruitment info page', () => {
   const wrapper = shallowMount(RecruitmentInfo, { localVue, router });
 
-  it('Pass an array of length 5 to getPageLength and should return 1', () => {
-    const arr = [1, 2, 3, 4, 5];
-    const length = wrapper.vm.getPageLength(arr);
-    expect(length).toBe(1);
-  });
-
-  it('Sort() method should return an array with latest date to the oldest', () => {
-    const arr = [
-      { id: '01', time: '2020-03-18' },
-      { id: '02', time: '2020-09-10' },
-      { id: '03', time: '2021-06-30' },
-      { id: '04', time: '2021-03-03' },
-    ];
-    const result = [
-      { id: '03', time: '2021-06-30' },
-      { id: '04', time: '2021-03-03' },
-      { id: '02', time: '2020-09-10' },
-      { id: '01', time: '2020-03-18' },
-    ];
-    const sortedArr = wrapper.vm.sort(arr);
-    expect(sortedArr).toStrictEqual(result);
-  });
-
   it('Filter should return an array that matches with corresponding tags', () => {
     const arr = [
       { id: '01', position: '技术类', city: '上海', type: '社会招聘' },
