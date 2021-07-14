379c127d58868c2152d5300769465482800f5920
fork1028 <xinyue_chen@icloud.com>
2021-07-14 11:57:08 +0800

    rewrite concat()

--- src/util.js
+++ src/util.js
@@ -3,7 +3,7 @@ export default {
     return Math.ceil(arr.length / 10);
   },
   sort(arr) {
-    const temp = arr.concat([]);
+    const temp = [...arr];
     temp.sort((a, b) => {
       const t1 = new Date(Date.parse(a.time.replace(/-/g, '/')));
       const t2 = new Date(Date.parse(b.time.replace(/-/g, '/')));
