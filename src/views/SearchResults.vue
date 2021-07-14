65cfd5deeea01a9fff5744fb0389f91da6131230
fork1028 <xinyue_chen@icloud.com>
2021-07-14 14:16:43 +0800

    responsive layout change for search results page

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -1,55 +1,58 @@
 <template>
   <div>
-    <v-row>
-      <v-col cols="2"></v-col>
-      <v-col cols="8">
-        <div class="search-results-title">{{ this.searchText }} -- 搜索结果</div>
-        <div class="search-results-filter">
-          <div class="search-results-num">
-            共找到{{ this.searchResults.length }}条结果, 该标签显示{{ this.filteredResults.length }}条结果
-          </div>
-          <v-row>
-            <v-chip-group class="filter-button" mandatory active-class="primary--text">
-              <v-chip
-                v-for="category in categories"
-                :key="category"
-                @click="filteredResults = filter(searchResults, category)"
-              >
-                {{ category }}
-              </v-chip>
-            </v-chip-group>
-          </v-row>
-        </div></v-col
-      >
-      <v-col cols="2"></v-col>
-    </v-row>
+    <v-app>
+      <v-container>
+        <v-row>
+          <v-col cols="1"></v-col>
+          <v-col cols="8">
+            <div class="search-results-title">{{ this.searchText }} -- 搜索结果</div>
+            <div class="search-results-filter">
+              <div class="search-results-num">
+                共找到{{ this.searchResults.length }}条结果, 该标签显示{{ this.filteredResults.length }}条结果
+              </div>
+              <v-row>
+                <v-chip-group class="filter-button" mandatory active-class="primary--text">
+                  <v-chip
+                    v-for="category in categories"
+                    :key="category"
+                    @click="filteredResults = filter(searchResults, category)"
+                  >
+                    {{ category }}
+                  </v-chip>
+                </v-chip-group>
+              </v-row>
+            </div></v-col
+          >
+        </v-row>
 
-    <div v-for="item in this.filteredResults.slice(pageNumber * 10 - 10, pageNumber * 10)" :key="item.id">
-      <v-card
-        class="search-results-card mx-auto pa-md-4"
-        max-width="1000"
-        :hover="true"
-        @click="link(item.tag, item.id)"
-      >
-        <v-card-text>
-          <p class="text-h4 text--primary">{{ item.title }}</p>
-          <p>{{ item.department }} | {{ item.time }}</p>
-          <v-row class="text--primary" no-gutters>
-            {{ item.responsibility || item.content }}
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </div>
+        <div v-for="item in this.filteredResults.slice(pageNumber * 10 - 10, pageNumber * 10)" :key="item.id">
+          <v-card
+            class="search-results-card mx-auto pa-md-4"
+            max-width="1000"
+            :hover="true"
+            @click="link(item.tag, item.id)"
+          >
+            <v-card-text>
+              <p class="text-h4 text--primary">{{ item.title }}</p>
+              <p>{{ item.department }} | {{ item.time }}</p>
+              <v-row class="text--primary" no-gutters>
+                {{ item.responsibility || item.content }}
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </div>
 
-    <div class="search-results-none" v-show="this.filteredResults.length === 0">暂无相关信息</div>
-    <div class="page-pagination" v-show="this.filteredResults.length !== 0">
-      <v-pagination
-        v-model="pageNumber"
-        total-visible="10"
-        :length="getPageLength(this.filteredResults)"
-        @input="handlePageChange"
-      ></v-pagination>
-    </div>
+        <div class="search-results-none" v-show="this.filteredResults.length === 0">暂无相关信息</div>
+        <div class="page-pagination" v-show="this.filteredResults.length !== 0">
+          <v-pagination
+            v-model="pageNumber"
+            total-visible="10"
+            :length="getPageLength(this.filteredResults)"
+            @input="handlePageChange"
+          ></v-pagination>
+        </div>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
