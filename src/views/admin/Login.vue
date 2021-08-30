389c2ef1c658780b54e0ca6deb59b86079bbb7f7
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-08-30 14:14:49 -0700

    completed login form for admin

--- src/views/admin/Login.vue
+++ src/views/admin/Login.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>Login</div>
+  <div class="form-container">
+    <img class="mb-5" width="150" src="../../assets/logo-black.svg" alt="logo image" />
+    <v-form ref="form" v-model="valid">
+      <label>Email</label>
+      <v-text-field v-model="email" type="email" required dense outlined></v-text-field>
+
+      <label>Password</label>
+      <v-text-field v-model="password" type="password" required dense outlined></v-text-field>
+
+      <v-btn color="primary" style="float: right">Login</v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      valid: false,
+      email: '',
+      password: '',
+    };
   },
 };
 </script>
+
+<style lang="scss" scpoed>
+.form-container {
+  align-items: center;
+  margin: auto;
+  width: 396px;
+  padding: 30px 20px;
+  background-color: white;
+  box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+  border-radius: 12px;
+}
+</style>
