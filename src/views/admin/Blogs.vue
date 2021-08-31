b6e89ea4cb42393260169b2831a16051b4b81f10
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-08-31 15:52:43 -0700

    added files and routes for create and modify blog

--- src/views/admin/Blogs.vue
+++ src/views/admin/Blogs.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="layout">
     <AdminNav />
-    <v-main> Blogs </v-main>
+    <v-main>
+      <v-data-table :headers="headers" :items="desserts" sort-by="modified" class="elevation-1" style="height: 100vh">
+        <template v-slot:top>
+          <v-toolbar flat color="white">
+            <v-toolbar-title>All Blogs</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <router-link to="/admin/blogs/create">
+              <v-btn color="primary" dark class="mb-2"> New Blog </v-btn>
+            </router-link>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+      </v-data-table>
+    </v-main>
   </div>
 </template>
 
@@ -9,12 +29,94 @@
 import AdminNav from '../../components/AdminNav.vue';
 
 export default {
-  data() {
-    return {};
-  },
   components: {
     AdminNav,
   },
+  data: () => ({
+    dialog: false,
+    headers: [
+      {
+        text: 'Title',
+        align: 'start',
+        value: 'title',
+      },
+      { text: 'Author', value: 'author' },
+      { text: 'Created', value: 'created' },
+      { text: 'Last Modified', value: 'modified' },
+      { text: 'Actions', value: 'actions', sortable: false },
+    ],
+    desserts: [
+      {
+        title: 'Frozen Yogurt',
+        author: 'AA',
+        created: '04.02',
+        modified: '06.12',
+      },
+      {
+        title: 'Amazing news',
+        author: 'AA',
+        created: '04.03',
+        modified: '06.25',
+      },
+    ],
+    editedIndex: -1,
+    editedItem: {
+      name: '',
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    defaultItem: {
+      name: '',
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+  }),
+
+  // computed: {
+  //   formTitle() {
+  //     return this.editedIndex === -1 ? 'New Item' : 'Edit Item';
+  //   },
+  // },
+
+  // watch: {
+  //   dialog(val) {
+  //     val || this.close();
+  //   },
+  // },
+
+  // methods: {
+  //   editItem(item) {
+  //     this.editedIndex = this.desserts.indexOf(item);
+  //     this.editedItem = Object.assign({}, item);
+  //     this.dialog = true;
+  //   },
+
+  //   deleteItem(item) {
+  //     const index = this.desserts.indexOf(item);
+  //     confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1);
+  //   },
+
+  //   close() {
+  //     this.dialog = false;
+  //     this.$nextTick(() => {
+  //       this.editedItem = Object.assign({}, this.defaultItem);
+  //       this.editedIndex = -1;
+  //     });
+  //   },
+
+  //   save() {
+  //     if (this.editedIndex > -1) {
+  //       Object.assign(this.desserts[this.editedIndex], this.editedItem);
+  //     } else {
+  //       this.desserts.push(this.editedItem);
+  //     }
+  //     this.close();
+  //   },
+  // },
 };
 </script>
 
