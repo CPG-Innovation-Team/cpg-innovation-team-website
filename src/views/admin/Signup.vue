5a782b4c20fb759058827dfdb6afaa9776ef32f9
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2021-09-07 14:21:47 -0700

    finished login and signup form

--- src/views/admin/Signup.vue
+++ src/views/admin/Signup.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="background">
     <div class="form-container">
-      <router-link to="/">
-        <img class="mb-5" width="150" src="../../assets/logo-black.svg" alt="logo image" />
-      </router-link>
-
+      <img class="mb-5" width="150" src="../../assets/logo-black.svg" alt="logo image" />
       <v-form ref="form" v-model="valid">
+        <label>Username</label>
+        <v-text-field v-model="username" type="email" required dense outlined></v-text-field>
+
         <label>Email</label>
         <v-text-field v-model="email" type="email" required dense outlined></v-text-field>
 
         <label>Password</label>
-        <v-text-field v-model="password" type="password" required hide-details dense outlined></v-text-field>
+        <v-text-field v-model="password" type="password" required dense outlined></v-text-field>
+
+        <label>Confirm Password</label>
+        <v-text-field v-model="confirmedPwd" type="password" required hide-details dense outlined></v-text-field>
 
         <p style="text-align: right; font-size: 0.9rem; margin-top: 8px">
-          <a>Forgot password?</a>
+          Already have an account? <router-link to="/login">Login</router-link>
         </p>
-        <p style="text-align: right; font-size: 0.9rem">
-          Don't have an account? <router-link to="/signup">Sign up</router-link>
-        </p>
-        <v-btn color="primary" style="float: right; margin-left: 100%">Login</v-btn>
+
+        <v-btn color="primary" style="float: right; margin-left: 100%">Signup</v-btn>
       </v-form>
     </div>
   </div>
@@ -29,8 +30,10 @@ export default {
   data() {
     return {
       valid: false,
+      username: '',
       email: '',
       password: '',
+      confirmedPwd: '',
     };
   },
 };
