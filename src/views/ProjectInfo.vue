43cdcb750f4ed8ebb5e1edf5f4202585067240bf
casualbill <hotwindzb@gmail.com>
2022-02-17 12:30:39 +0800

    Revert "Update project info content for mobile"
    This reverts commit 8ac2b88f4b8d2e4a0fa2e7380b674d7a6276778e.

--- src/views/ProjectInfo.vue
+++ src/views/ProjectInfo.vue
@@ -57,9 +57,8 @@
             <v-expand-transition>
               <v-card v-show="project.show">
                 <v-card-text>{{ project.intro }}</v-card-text>
-                <v-chip v-for="(item, j) in project.list" :key="j" color="blue" class="chip-link">
-                  <a :href="item.link">{{ item.name }}</a>
-                </v-chip>
+                <v-card-subtitle>项目</v-card-subtitle>
+                <v-chip v-for="(link, j) in project.list" :key="j" color="blue" class="chip-link"> {{ link }} </v-chip>
               </v-card>
             </v-expand-transition>
           </v-row>
@@ -167,7 +166,6 @@ h1 {
   .header {
     color: white;
     margin-top: 5%;
-    text-shadow: 1px 1px 2px black;
   }
   .chip-show {
     float: right;
@@ -178,11 +176,6 @@ h1 {
     color: white;
     margin: 10px;
     padding: 10px 20px;
-
-    a {
-      color: #fff;
-      text-decoration: none;
-    }
   }
 }
 
