9513c78d4dfd55268db1622f6e275fe36a771ba7
fork1028 <xinyue_chen@icloud.com>
2022-03-23 20:10:54 -0400

    update signup unit test with locale

--- tests/unit/views/admin/Signup.spec.js
+++ tests/unit/views/admin/Signup.spec.js
@@ -1,13 +1,31 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import Signup from '@/views/admin/Signup.vue';
 import Vue from 'vue';
+import i18n from '@/plugins/vue-i18n';
+import VueI18n from 'vue-i18n';
+import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
+Vue.use(VueI18n);
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify();
+const router = new VueRouter();
 
 describe('User sign up page', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(Signup, {
+      mocks: {
+        $t: (msg) => i18n.messages['zh-CN'][msg],
+      },
+      stubs: ['router-link', 'router-view'],
+      vuetify,
+      router,
+    });
+  });
   it('Axios is rendered', () => {
-    const wrapper = shallowMount(Signup);
     expect(wrapper.exists()).toBe(true);
   });
 });
