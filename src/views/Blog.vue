41874d60d0a593ab60dbaccf76761846a9e1331b
fork1028 <xinyue_chen@icloud.com>
2022-03-29 17:11:31 -0400

    remove toString() on sn

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -19,7 +19,7 @@
               <div v-for="(blog, index) in popularBlogs.slice(0, 4)" :key="index">
                 <v-col>
                   <v-card class="ml-2 mr-2" flat color="transparent" max-width="210">
-                    <router-link :to="{ path: '/blogDetail', query: { sn: blog.sn.toString() } }">
+                    <router-link :to="{ path: '/blogDetail', query: { sn: blog.sn } }">
                       <img
                         class="popular-blog-cover"
                         v-if="blog.imgIsValid"
@@ -35,10 +35,7 @@
                       />
                     </router-link>
                     <v-card-title>
-                      <router-link
-                        class="popular-blog-title"
-                        :to="{ path: '/blogDetail', query: { sn: blog.sn.toString() } }"
-                      >
+                      <router-link class="popular-blog-title" :to="{ path: '/blogDetail', query: { sn: blog.sn } }">
                         <div>{{ getBlogTitle(blog.title) }}</div>
                       </router-link></v-card-title
                     >
