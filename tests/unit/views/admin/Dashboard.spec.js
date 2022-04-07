0ffc7d41bf7a336ec81587c0cc74ded85ef5fbbd
fork1028 <xinyue_chen@icloud.com>
2022-04-07 19:44:14 -0400

    update dashboard unit test

--- tests/unit/views/admin/Dashboard.spec.js
+++ tests/unit/views/admin/Dashboard.spec.js
@@ -1,9 +1,14 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Dashboard from '@/views/admin/Dashboard.vue';
+import VueRouter from 'vue-router';
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+const router = new VueRouter();
 
 describe('Admin dashboard page', () => {
   it('The adminNav bar is rendered', () => {
-    const wrapper = shallowMount(Dashboard);
+    const wrapper = shallowMount(Dashboard, { localVue, router });
     expect(wrapper.exists()).toBe(true);
   });
 });
