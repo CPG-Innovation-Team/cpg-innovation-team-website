96dbf69958cbbf82c74a1ce09020910c912e39c7
fork1028 <xinyue_chen@icloud.com>
2022-04-07 19:43:54 -0400

    add accessDenied page layout

---
+++ src/views/admin/AccessDenied.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="layout">
+    <AdminNav />
+    <v-main style="background: whitesmoke">
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+          <v-card elevation="0" color="transparent">
+            <v-row align="center" justify="center">
+              <v-icon x-large>mdi-alert-circle</v-icon>
+              <v-card-title>Sorry, you don't have the access</v-card-title>
+            </v-row>
+          </v-card>
+        </v-row>
+      </v-container>
+    </v-main>
+  </div>
+</template>
+
+<script>
+import AdminNav from '../../components/AdminNav.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    AdminNav,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+}
+</style>
