c398537063d0e6befbddc0936dd37506099f0941
fork1028 <xinyue_chen@icloud.com>
2022-04-20 14:08:51 -0400

    add locale for admin user

--- tests/unit/views/admin/Users.spec.js
+++ tests/unit/views/admin/Users.spec.js
@@ -1,18 +1,35 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Users from '@/views/admin/Users.vue';
-import VueRouter from 'vue-router';
 import Vue from 'vue';
+import i18n from '@/plugins/vue-i18n';
+import Vuetify from 'vuetify';
+import VueI18n from 'vue-i18n';
+import VueRouter from 'vue-router';
+
+Vue.use(VueI18n);
+Vue.use(Vuetify);
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
 
 const router = new VueRouter();
-
-Vue.use(VueRouter);
+const vuetify = new Vuetify();
 
 jest.mock('axios', () => ({
   post: jest.fn(() => Promise.resolve({ data: {} })),
 }));
 
 describe('Admin users page', () => {
-  const wrapper = shallowMount(Users, { router });
+  const wrapper = shallowMount(Users, {
+    mocks: {
+      $t: (msg) => i18n.messages['zh-CN'][msg],
+    },
+    stubs: ['router-link', 'router-view'],
+    vuetify,
+    localVue,
+    router,
+  });
+
   it('The adminNav is rendered', () => {
     expect(wrapper.exists()).toBe(true);
   });
