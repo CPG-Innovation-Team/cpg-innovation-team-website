c2d6394dde07e847eb1d7c590571a2e4a7d59d46
fork1028 <xinyue_chen@icloud.com>
2022-05-09 20:55:03 -0400

    remove duplicate code

--- src/components/RollingGallery.vue
+++ src/components/RollingGallery.vue
@@ -14,21 +14,8 @@
         >
           <v-hover v-slot="{ hover }">
             <div class="gallery-asset-container">
-              <div v-if="item.image">
-                <v-img :src="item.image" class="gallery-image"
-                  ><v-fade-transition>
-                    <v-overlay v-if="hover" absolute color="black">
-                      <div class="d-flex gallery-item-description-text">
-                        <v-avatar color="yellow" size="36"></v-avatar>
-                      </div>
-                      <div class="d-flex gallery-item-description-text">{{ item.text }}</div>
-                      <div class="d-flex gallery-item-description-text">Some description</div>
-                    </v-overlay>
-                  </v-fade-transition></v-img
-                >
-              </div>
-              <div v-if="item.gif">
-                <v-img :src="item.gif" width="100%">
+              <div v-if="item.type === 'gif' || item.type === 'image'">
+                <v-img :src="item.url" width="100%">
                   <v-fade-transition>
                     <v-overlay v-if="hover" absolute color="black">
                       <div class="d-flex gallery-item-description-text">
@@ -41,12 +28,16 @@
                 </v-img>
                 <v-btn class="gallery-item-marker" x-small> <v-icon x-small>mdi-alpha-g-box</v-icon></v-btn>
               </div>
-              <div v-if="item.video" @mouseover="autoplayVideo(item.text)" @mouseleave="autopauseVideo(item.text)">
+              <div
+                v-if="item.type === 'video'"
+                @mouseover="autoplayVideo(item.text)"
+                @mouseleave="autopauseVideo(item.text)"
+              >
                 <v-row>
                   <video
                     :ref="item.text"
                     :id="item.text"
-                    :src="item.video"
+                    :src="item.url"
                     width="100%"
                     loop
                     preload="auto"
@@ -66,20 +57,12 @@
             </div>
           </v-hover>
           <v-row class="pt-2 pb-2 pl-4 pr-4" align="center" no-gutters>
-            <v-col cols="2"><v-avatar class="gallery-avatar" color="yellow" size="20"> </v-avatar></v-col>
+            <v-col cols="2"><v-avatar class="gallery-avatar" color="yellow" size="20"></v-avatar></v-col>
             <v-col cols="8">
               <div class="gallery-text">{{ item.text }}</div>
             </v-col>
             <v-col cols="2" class="text-right">
-              <div v-if="item.country === `China`">
-                <span class="fi fi-cn"></span>
-              </div>
-              <div v-if="item.country === `Thailand`">
-                <span class="fi fi-th"></span>
-              </div>
-              <div v-if="item.country === `England`">
-                <span class="fi fi-gb"></span>
-              </div>
+              <span :class="countryTable[item.country]"></span>
             </v-col>
           </v-row>
         </v-card>
@@ -97,54 +80,67 @@ export default {
     GalleryLabel,
   },
   data: () => ({
+    countryTable: {
+      China: 'fi fi-cn',
+      Thailand: 'fi fi-th',
+      England: 'fi fi-gb',
+    },
     galleryData: [
       {
         flex: 2,
         data: [
           {
-            image: 'https://static.cpgroup.top/product/pocky/img-pocky1.png',
+            url: 'https://static.cpgroup.top/product/pocky/img-pocky1.png',
+            type: 'image',
             text: 'POCKY 巧克力味',
             label: '精选',
             country: 'China',
           },
           {
-            gif: 'https://media3.giphy.com/media/bsfUpZfjw8pBr2QKUg/giphy.gif',
+            url: 'https://media3.giphy.com/media/bsfUpZfjw8pBr2QKUg/giphy.gif',
+            type: 'gif',
             text: 'test gif',
             label: '',
             country: 'England',
           },
           {
-            image: 'https://static.cpgroup.top/product/pocky/img-pocky2.png',
+            url: 'https://static.cpgroup.top/product/pocky/img-pocky2.png',
+            type: 'image',
             text: 'POCKY 草莓味',
             label: '限定',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/pocky/img-pocky3.png',
+            url: 'https://static.cpgroup.top/product/pocky/img-pocky3.png',
+            type: 'image',
             text: 'POCKY 杏仁味',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/pocky/img-pocky4.png',
+            url: 'https://static.cpgroup.top/product/pocky/img-pocky4.png',
+            type: 'image',
             text: 'POCKY 巧克力香蕉味',
             label: '销量最佳',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/pocky/img-pocky5.png',
+            url: 'https://static.cpgroup.top/product/pocky/img-pocky5.png',
+            type: 'image',
             text: 'POCKY 香草莓味',
             label: '搜索第一',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/pocky/img-pocky6.png',
+            url: 'https://static.cpgroup.top/product/pocky/img-pocky6.png',
+            type: 'image',
             text: 'POCKY 抹茶味',
             label: '新品',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/pocky/img-pocky7.png',
+            url: 'https://static.cpgroup.top/product/pocky/img-pocky7.png',
+            type: 'image',
             text: 'POCKY 奶油曲奇味',
             label: '',
             country: 'Thailand',
@@ -155,56 +151,64 @@ export default {
         flex: 4,
         data: [
           {
-            video:
-              'https://static.videezy.com/system/resources/previews/000/041/925/original/C0008_2016-11-16_12-48.mp4',
+            url: 'https://static.videezy.com/system/resources/previews/000/041/925/original/C0008_2016-11-16_12-48.mp4',
+            type: 'video',
             text: 'test video',
             label: '限定',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft1.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft1.png',
+            type: 'image',
             text: 'NFT Series 1',
             label: '限定',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft2.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft2.png',
+            type: 'image',
             text: 'NFT Series 2',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft3.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft3.png',
+            type: 'image',
             text: 'NFT Series 3',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft4.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft4.png',
+            type: 'image',
             text: 'NFT Series 4',
             label: '精选',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft5.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft5.png',
+            type: 'image',
             text: 'NFT Series 5',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft6.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft6.png',
+            type: 'image',
             text: 'NFT Series 6',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft7.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft7.png',
+            type: 'image',
             text: 'NFT Series 7',
             label: '搜索第一',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/nft/img-nft8.png',
+            url: 'https://static.cpgroup.top/product/nft/img-nft8.png',
+            type: 'image',
             text: 'NFT Series 8',
             label: '',
             country: 'China',
@@ -215,55 +219,64 @@ export default {
         flex: 2,
         data: [
           {
-            video: 'https://static.videezy.com/system/resources/previews/000/040/953/original/5_04_10_19.mp4',
+            url: 'https://static.videezy.com/system/resources/previews/000/040/953/original/5_04_10_19.mp4',
+            type: 'video',
             text: 'test video 2',
             label: '限定',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water1.png',
+            url: 'https://static.cpgroup.top/product/water/img-water1.png',
+            type: 'image',
             text: '纯悦矿泉水',
             label: '销量最佳',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water2.png',
+            url: 'https://static.cpgroup.top/product/water/img-water2.png',
+            type: 'image',
             text: '华润怡宝饮用纯净水',
             label: '销量最佳',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water3.png',
+            url: 'https://static.cpgroup.top/product/water/img-water3.png',
+            type: 'image',
             text: '今麦郎饮用天然水',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water4.png',
+            url: 'https://static.cpgroup.top/product/water/img-water4.png',
+            type: 'image',
             text: '景田百岁山天然矿泉水',
             label: '',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water5.png',
+            url: 'https://static.cpgroup.top/product/water/img-water5.png',
+            type: 'image',
             text: '均瑶恩赐饮用天然矿泉水',
             label: '',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water6.png',
+            url: 'https://static.cpgroup.top/product/water/img-water6.png',
+            type: 'image',
             text: '康师傅包装饮用水',
             label: '精选',
             country: 'England',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water7.png',
+            url: 'https://static.cpgroup.top/product/water/img-water7.png',
+            type: 'image',
             text: '农夫山泉饮用天然水',
             label: '限定',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/water/img-water8.png',
+            url: 'https://static.cpgroup.top/product/water/img-water8.png',
+            type: 'image',
             text: '依云 evian 天然矿泉水',
             label: '限定',
             country: 'England',
@@ -274,43 +287,50 @@ export default {
         flex: 3,
         data: [
           {
-            image: 'https://static.cpgroup.top/product/dumpling/img-dumpling1.png',
+            url: 'https://static.cpgroup.top/product/dumpling/img-dumpling1.png',
+            type: 'image',
             text: '鸡肉时蔬饺',
             label: '搜索第一',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/dumpling/img-dumpling2.png',
+            url: 'https://static.cpgroup.top/product/dumpling/img-dumpling2.png',
+            type: 'image',
             text: '椒麻鸡肉饺',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/dumpling/img-dumpling3.png',
+            url: 'https://static.cpgroup.top/product/dumpling/img-dumpling3.png',
+            type: 'image',
             text: '青菜鲜肉饺',
             label: '限定',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/dumpling/img-dumpling4.png',
+            url: 'https://static.cpgroup.top/product/dumpling/img-dumpling4.png',
+            type: 'image',
             text: '墨鱼芹香饺',
             label: '',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/dumpling/img-dumpling5.png',
+            url: 'https://static.cpgroup.top/product/dumpling/img-dumpling5.png',
+            type: 'image',
             text: '日式饺子',
             label: '新品',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/dumpling/img-dumpling6.png',
+            url: 'https://static.cpgroup.top/product/dumpling/img-dumpling6.png',
+            type: 'image',
             text: '香菜肉圆水饺',
             label: '精选',
             country: 'China',
           },
           {
-            image: 'https://static.cpgroup.top/product/dumpling/img-dumpling7.png',
+            url: 'https://static.cpgroup.top/product/dumpling/img-dumpling7.png',
+            type: 'image',
             text: '青瓜虾仁水饺',
             label: '精选',
             country: 'China',
@@ -321,61 +341,71 @@ export default {
         flex: 2,
         data: [
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product1.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product1.png',
+            type: 'image',
             text: '森蜂园-蜂王浆',
             label: '',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product2.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product2.png',
+            type: 'image',
             text: '蜂胶银杏软胶囊',
             label: '',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product3.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product3.png',
+            type: 'image',
             text: '茶花蜂花粉',
             label: '',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product4.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product4.png',
+            type: 'image',
             text: '长白山椴树原蜜',
             label: '限定',
             country: 'Thailand',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product5.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product5.png',
+            type: 'image',
             text: '1000米长白山椴树原...',
             label: '搜索第一',
             country: 'England',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product6.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product6.png',
+            type: 'image',
             text: '多花蜂蜜 （勺蜜）',
             label: '',
             country: 'England',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product7.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product7.png',
+            type: 'image',
             text: '长白山紫椴蜂蜜（勺蜜）',
             label: '',
             country: 'England',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product8.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product8.png',
+            type: 'image',
             text: '长白山椴树蜂蜜礼盒',
             label: '新品',
             country: 'England',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product9.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product9.png',
+            type: 'image',
             text: '森蜂园上海蜂蜜礼盒',
             label: '限定',
             country: 'England',
           },
           {
-            image: 'https://static.cpgroup.top/product/honey/img-product10.png',
+            url: 'https://static.cpgroup.top/product/honey/img-product10.png',
+            type: 'image',
             text: '麦卢卡 MANUKA 蜂蜜礼盒',
             label: '',
             country: 'England',
