4c72e5f9b1ed682f00ac3d5ffc8d158dbdda0aba
fork1028 <xinyue_chen@icloud.com>
2022-07-12 17:46:47 -0400

    add image upload feature in editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -11,17 +11,23 @@
 
 <script>
 import Editor from '@tinymce/tinymce-vue';
+import util from '../util';
 
 export default {
+  components: {
+    Editor,
+  },
   props: {
-    content: {
+    prevContent: {
       type: String,
     },
-    editorBool: {
+    toolbar: {
       type: Boolean,
+      default: true,
     },
-    inCreate: {
+    inCreatePage: {
       type: Boolean,
+      default: false,
     },
   },
   data() {
@@ -43,34 +49,62 @@ export default {
         toolbar:
           'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | lists image | removeformat | help',
         init_instance_callback: (editor) => {
-          if (this.value) {
-            editor.setContent(this.value);
-          }
-          this.hasInit = true;
           editor.on('NodeChange Change KeyUp SetContent', () => {
-            this.hasChange = true;
             this.$emit('input', editor.getContent());
-            if (this.inCreate === true) {
+            if (this.inCreatePage === true) {
               this.editingContent = editor.getContent();
               clearTimeout(this.timer);
-              this.timer = setTimeout(this.saveContent, 2000);
+              this.timer = setTimeout(this.saveContent, 1000);
             }
           });
+        },
+        images_upload_handler: (blobInfo, success, failure) => {
+          const formatIsValid =
+            blobInfo.blob().type === 'image/jpeg' ||
+            blobInfo.blob().type === 'image/png' ||
+            blobInfo.blob().type === 'image/GIF' ||
+            blobInfo.blob().type === 'image/jpg';
+          if (blobInfo.blob().size / 1024 / 1024 > 5) {
+            if (this.$t('locale') === 'zh-CN') {
+              failure('上传失败，图片大小请控制在 5MB 以内');
+            } else {
+              failure('Failed to upload image, the maximum size supported is 5MB');
+            }
+          } else if (!formatIsValid) {
+            if (this.$t('locale') === 'zh-CN') {
+              failure('仅支持jpeg/jpg/png/GIF格式');
+            } else {
+              failure('Only jpeg/jpg/png/GIF formats are supported');
+            }
+          } else {
+            const formData = new FormData();
+            formData.append('file', blobInfo.blob(), blobInfo.filename());
+            util
+              .post('https://api.cpgroup.top:8080/admin/object/upload', formData, this.$router)
+              .then((res) => {
+                success(res.data.data.url);
+              })
+              .catch(() => {
+                if (this.$t('locale') === 'zh-CN') {
+                  failure('上传失败');
+                } else {
+                  failure('Failed to upload');
+                }
+              });
+          }
         },
       },
     };
   },
-  components: {
-    Editor,
-  },
   created() {
-    if (this.editorBool === false) {
+    if (this.toolbar === false) {
       this.init.menubar = false;
       this.init.toolbar = false;
       this.disabled = true;
     }
-    if (this.content) {
-      this.blogContent = this.content;
+    // restore blog content from local storage
+    if (this.prevContent) {
+      this.blogContent = this.prevContent;
     }
   },
   methods: {
@@ -79,7 +113,8 @@ export default {
     },
   },
   watch: {
-    content(newVal) {
+    // watch blog content in async/await (updateBlog and ApproveBlog)
+    prevContent(newVal) {
       this.blogContent = newVal;
     },
   },
